ece5bb0570322f00155719beb83b48c386c8e51b
BasselAlshayeb <basl.alshayeb@gmail.com>
2025-05-10 02:31:46 +0300

    added api for testing

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,4 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-// Webhooks routes
-Route::get('/test', [\App\Http\Controllers\AmoController::class, 'test']);
 Route::post('/webhook', [\App\Http\Controllers\AmoController::class, 'webhook'])->name('amocrm.webhook');
